49f1a0b1d3f553bef2bab19b4ffb8a9f577f7fae
Wei Shi <swsmile1028@gmail.com>
2018-06-15 15:08:57 +0100

    - Add json parser dependency - Add controller code - Add bean

--- initial/src/main/java/hello/HelloController.java
+++ initial/src/main/java/hello/HelloController.java
@@ -1,14 +1,74 @@
 package hello;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
-    
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    Environment environment;
+
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+
+        ParkingSpace parkingSpace = new ParkingSpace();
+
+
+        String port =environment.getProperty("local.server.port");
+        System.out.println("Current port:" + port);
+        logger.info(String.valueOf(port == "8081"));
+
+        if (port.equals("8081")){
+            parkingSpace.setId("1");
+            parkingSpace.setName("Velvet House");
+            parkingSpace.setLat(53.476303);
+            parkingSpace.setLongti(-2.235134);
+        }
+        else if (port.equals("8082")){
+            parkingSpace.setId("2");
+            parkingSpace.setName("The Assembly Apartments");
+            parkingSpace.setLat(53.474018);
+            parkingSpace.setLongti(-2.243838);
+        }
+        else if (port.equals("8083")){
+            parkingSpace.setId("3");
+            parkingSpace.setName("Cotswold Outdoor Manchester Deansgate");
+            parkingSpace.setLat(53.481575);
+            parkingSpace.setLongti(-2.247807);
+        }
+        else if (port.equals("8084")){
+            parkingSpace.setId("4");
+            parkingSpace.setName("University of Salford");
+            parkingSpace.setLat(53.486979);
+            parkingSpace.setLongti(-2.271241);
+        }
+        else{
+            parkingSpace.setId("5");
+            parkingSpace.setName("National Football Museum");
+            parkingSpace.setLat(53.487840);
+            parkingSpace.setLongti(-2.242093);
+        }
+
+        String json = "";
+        try{
+            ObjectMapper mapper = new ObjectMapper();
+            json = mapper.writeValueAsString(parkingSpace);
+
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+
+        return json;
     }
     
 }
---
+++ initial/src/main/java/hello/ParkingSpace.java
@@ -0,0 +1,61 @@
+package hello;
+
+
+public class ParkingSpace {
+    
+    private String id;
+    private String name;
+    private double lat;
+    private double longi;
+
+    public Boolean getAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(Boolean available) {
+        isAvailable = available;
+    }
+
+    private Boolean isAvailable;
+
+
+    public ParkingSpace(){
+        this.isAvailable = Math.random() < 0.5;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLongi() {
+        return longi;
+    }
+
+    public void setLongti(double longt) {
+        this.longi = longt;
+    }
+
+
+    
+}
