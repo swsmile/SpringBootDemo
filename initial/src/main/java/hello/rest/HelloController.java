e2bd0e9748af5e3a777a3e5e217440b012b78a02
Wei Shi <swsmile1028@gmail.com>
2018-06-24 11:10:34 +0100

    Spring Boot only version

--- initial/src/main/java/hello/rest/HelloController.java
+++ initial/src/main/java/hello/rest/HelloController.java
@@ -1,24 +1,4 @@
-package hello.rest;
-
-import java.text.DecimalFormat;
-import java.util.Collection;
-import java.util.Random;
-
-import java.text.DecimalFormat;
-import java.util.Collection;
-import java.util.Random;
-
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
+package hello;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -26,15 +6,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.text.DecimalFormat;
+import java.util.Random;
 
-import hello.ParkingSpace;
-import hello.StrGenerator;
-
-//@RestController
-@Path("/state")
-@Component
+@RestController
 public class HelloController {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -44,10 +22,8 @@ public class HelloController {
     @Autowired
     Environment environment;
 
-//    @RequestMapping("/state")
-    @GET
-    @Produces({ MediaType.APPLICATION_JSON })
-    public Response index() {
+    @RequestMapping("/state")
+    public String index() {
 
         ParkingSpace parkingSpace = new ParkingSpace();
 
@@ -107,12 +83,9 @@ public class HelloController {
             e.printStackTrace();
         }
 
-        return Response.status(Response.Status.OK).entity(parkingSpace).build();
-//        return json;
+
+        return json;
     }
-
-
-
 
     private static double getRandomLat(){
         double lat = -2.25 + rand.nextDouble() * 0.04;
